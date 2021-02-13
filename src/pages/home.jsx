10f01eb0f74ead39b9a26593810cdbef36b4da54
Félix Bouveret <bouveret.felix@gmail.com>
2021-02-13 22:23:18 +0100

    Fix eslit errors

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import HomeModule from "../module/Home";
 
 export default function Home() {
--- src/pages/quiz.jsx
+++ src/pages/quiz.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import QuizModule from "../module/Quiz";
 
 export default function Quiz() {
