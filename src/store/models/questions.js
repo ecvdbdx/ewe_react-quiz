9511f90ea3bf27a5574a8190f1920ebe600fd85d
Félix Bouveret <bouveret.felix@gmail.com>
2021-02-17 11:08:06 +0100

    Add rematch store

---
+++ src/store/models/questions.js
@@ -0,0 +1,40 @@
+const questions = {
+  state: {
+    questionsList: [],
+    answersList: [],
+  }, // initial state
+  reducers: {
+    // handle state changes with pure functions
+    addAnswer(state, payload) {
+      return {
+        ...state,
+        answersList: [...state.answersList, payload],
+      };
+    },
+    setQuestions(state, payload) {
+      console.log("reducer", payload);
+      return {
+        ...state,
+        questionsList: payload,
+      };
+    },
+  },
+  effects: (dispatch) => ({
+    // handle state changes with impure functions.
+
+    setQuestionsAsync(id, rootState) {
+      fetch(
+        `https://opentdb.com/api.php?amount=10&category=${id}&difficulty=easy`
+      )
+        .then((res) => res.json())
+        .then((res) => {
+          dispatch.questions.setQuestions(res.results);
+        })
+        .then(() => {
+          console.log(rootState);
+        });
+    },
+  }),
+};
+
+export default questions;
