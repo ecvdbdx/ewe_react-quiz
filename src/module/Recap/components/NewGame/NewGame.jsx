7d0b6b8e4442bc4ef3862a7b176a0d2f8725a6ee
Félix Bouveret <bouveret.felix@gmail.com>
2021-02-25 16:40:52 +0100

    Add newGame buttons

--- src/module/Recap/components/NewGame/NewGame.jsx
+++ src/module/Recap/components/NewGame/NewGame.jsx
@@ -1,9 +1,31 @@
 import "./NewGame.scss";
 import React from "react";
 import { connect } from "react-redux";
+import Button from "components/Button";
+import { useHistory } from "react-router-dom";
 
-const NewGameTemplate = ({ options }) => {
-  console.log(options);
+const NewGameTemplate = ({
+  options,
+  setQuestionsAsync,
+  openNotification,
+  openLoader,
+  closeLoader,
+}) => {
+  const history = useHistory();
+
+  const startQuizz = () => {
+    openLoader();
+    setQuestionsAsync()
+      .then(() => {
+        history.push("/quiz");
+      })
+      .catch((e) => {
+        openNotification({ title: "Error", message: e.message });
+      })
+      .then(() => {
+        closeLoader();
+      });
+  };
 
   return (
     <div className="newGameRoot">
@@ -12,6 +34,8 @@ const NewGameTemplate = ({ options }) => {
         <li>Difficulty : {options.difficultySelected || "Any"}</li>
         <li>Category : {options.name}</li>
       </ul>
+      <Button click={() => history.push("/")}>Back home</Button>
+      <Button click={() => startQuizz()}>Retry with these options</Button>
     </div>
   );
 };
@@ -20,4 +44,11 @@ const mapState = (state) => ({
   options: state.questions.options,
 });
 
-export default connect(mapState)(NewGameTemplate);
+const mapDispatch = ({ questions, notification, loader }) => ({
+  setQuestionsAsync: questions.setQuestionsAsync,
+  openNotification: notification.openNotification,
+  openLoader: loader.openLoader,
+  closeLoader: loader.closeLoader,
+});
+
+export default connect(mapState, mapDispatch)(NewGameTemplate);
